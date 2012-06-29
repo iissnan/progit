1dde2aaa529418c96c0d3a6170252d66266df27c
UIc <iissnan@gmail.com>
2012-06-29 17:39:50 +0800

    Server is running

---
+++ server.js
@@ -0,0 +1,12 @@
+var http = require('http');
+
+
+var server = http.createServer().listen(8000);
+
+server.on('request', function(req, res){
+    res.writeHead(200);
+    res.write('ProGit');
+    res.end();
+});
+
+console.log('ProGit Server Running on the port 8000');
