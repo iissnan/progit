acbcddb820d083438c5c51c0f943525a65c84d3a
UIc <iissnan@gmail.com>
2012-06-30 11:18:50 +0800

    Generate Contents Done

--- server.js
+++ server.js
@@ -1,29 +1,21 @@
-var http = require('http');
+var express = require('express');
 var path = require('path');
 var fs = require('fs');
 
 // the directory name of pro git repository
 var DIR_NAME_PGREPO = 'ProGitRepository';
 
-var server = http.createServer().listen(8000);
-
-server.on('request', function(req, res){
+var server = express.createServer().listen(8000);
+server.get('/', function(req, res){
     var dir_zh = __dirname + '/' + DIR_NAME_PGREPO + '/zh/';
-    fs.readdir(dir_zh, function(err, files){
-        if (!err) {
-            res.writeHead(200);
-            for (var i = 0; i < files.length; i++) {
-                var filePath = path.join(dir_zh, files[i]);
-                if (fs.statSync(filePath).isDirectory() ) {
-                    res.write(files[i] + '\n');
-                }
+        fs.readdir(dir_zh, function(err, files){
+            if (!err) {
+                res.render('index.jade', {Contents : files, layout : false});
+            } else {
+                res.writeHead(500);
+                res.end();
             }
-            res.end();
-        } else {
-            res.writeHead(500);
-            res.end();
-        }
-    });
+        });
 });
 
 console.log('ProGit Server Running on the port 8000');
