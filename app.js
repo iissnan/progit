b3a50193d30f220ad30f1326ce6e5bc70f029876
IIssNan <iissnan@gmail.com>
2013-02-07 15:04:36 +0800

    重构，完成章节列表的获取

--- app.js
+++ app.js
@@ -7,31 +7,24 @@ var DIR_NAME_PGREPO = 'ProGitRepository';
 var PATH_PGREPO = __dirname + '/' + DIR_NAME_PGREPO + '/';
 var PATH_FIGURE = PATH_PGREPO + 'figures/';
 
-
-// Start a Http Server, listen on port 8000
-var app = express.createServer().listen(8000);
-
+var app = express();
 
 
 /* ==================================
  * 服务配置
  * ================================== */
-app.configure(function(){
-    app.set("view engine", "jade");
-    app.set('views', __dirname + '/views');
-    app.use(express.static(__dirname + "/public"));
-});
+var oneYear = 31557600000;
+app.set("view engine", "jade");
+app.set('views', __dirname + '/views');
+app.use(express.static(__dirname + "/public"));
+app.use(express.favicon(__dirname + "/public/favicon.ico"), {maxAge : oneYear});
+
 app.configure("development", function(){
-    app.use(express.favicon(__dirname + "/public/favicon.ico"));
     app.use(express.errorHandler({dumpException:true, showStack: true}));
 
 });
 app.configure("production", function(){
-    var oneYear = 31557600000;
-    app.use(express.static(__dirname + "/public"));
-    app.use(express.favicon(__dirname + "/public/favicon.ico"), {maxAge : oneYear});
     app.use(express.errorHandler());
-    app.set("view engine", "jade");
 });
 
 
@@ -43,10 +36,8 @@ app.configure("production", function(){
 // 图片地址
 app.get('/figures/:figure_name', function(req, res){
     var figureName = req.params.figure_name;
-    console.log(figureName);
     if (figureName) {
         var filePath = path.join(PATH_FIGURE, figureName);
-        console.log(filePath);
         fs.readFile(filePath, function(err, data){
             if (!err) {
                 res.contentType(filePath);
@@ -58,44 +49,116 @@ app.get('/figures/:figure_name', function(req, res){
     }
 });
 
-app.get("/:controller?/:method?", function(req, res, next){
-    console.log("Request url: " + req.url);
 
-    var controller = req.params.controller,
-        method = req.params.method;
 
-    if (typeof controller === "undefined") {
-        showContents("zh", res);
-    } else if (controller === "about") {
-        res.render("about");
-    } else if (typeof controller !== "undefined"){
-        if (typeof method === "undefined") {
-            showContents(controller, res);
-        } else {
-            // get files
-        }
+app.get("/", function (req, res) {
+    // 默认现实中文章节列表
+    showChapterList("zh", res);
+});
+
+app.get("/about", function (req, res) {
+   res.render("about");
+});
+
+app.get("/:translate", function (req, res, next) {
+    var translate = req.params.translate;
+    var availableTranslate = ["en", "zh", "zh-tw"];
+    if (availableTranslate.indexOf(translate) === -1) {
+        res.send(404);
     } else {
-        next();
+        showChapterList(translate, res);
     }
 });
 
-/* 404 */
+app.get("/:translate/:chapter", function (req, res, next) {
+
+});
+
+/* Not Routes matched, show 404 */
 app.get("*", function(req, res){
     res.send("Nothing Found", 404);
 });
 
-function showContents(language, res){
-    fs.exists(path.join(PATH_PGREPO, language), function(exists){
+
+/* ==================================
+ * 逻辑处理
+ * ================================== */
+
+ /**
+ * 显示章节列表
+ * @param translate
+ * @param res
+ */
+function showChapterList(translate, res){
+    fs.exists(path.join(PATH_PGREPO, translate), function(exists){
         if (exists) {
-            res.render('index', {contents : [1,2]});
+            res.render('index', {chapters : getChapterList(translate)});
         } else {
             res.send(404);
         }
     });
 }
 
-function getFile(file){
+/**
+ * 获取translate下的章节
+ * @param translate
+ * @return {*}
+ */
+function getChapterList(translate){
+    var translateDirectory = path.join(PATH_PGREPO, translate);
+    var chapterMapping = {
+        "01-introduction": {
+            "en" : "Introduction",
+            "zh" : "起步"
+        },
+        "02-git-basics" : {
+            "en" : "Git Basics",
+            "zh" : "Git 基础"
+        },
+        "03-git-branching" : {
+            "en" : "Git Branching",
+            "zh" : "Git 分支"
+        },
+        "04-git-server" : {
+            "en" : "Git on the Server",
+            "zh" : "服务器上的 Git"
+        },
+        "05-distributed-git": {
+            "en" : "Distributed Git",
+            "zh" : "分布式 Git"
+        },
+        "06-git-tools" : {
+            "en" : "Git Tools",
+            "zh" : "Git 工具"
+        },
+        "07-customizing-git": {
+            "en" : "Customizing Git",
+            "zh" : "自定义 Git"
+        },
+        "08-git-and-other-scms" : {
+            "en" : "Git and Other Systems",
+            "zh" : "Git 与其他系统"
+        },
+        "09-git-internals": {
+            "en" : "Git Internals",
+            "zh" : "Git 内部原理"
+        }
+    };
+    var chapters = fs.readdirSync(translateDirectory);
+    chapters = chapters.map(function(chapter, i) {
+        return i + " " + chapterMapping[chapter][translate];
+    });
+
+    return chapters;
+}
+
+/**
+ * 获取章节内容
+ * @param file
+ */
+function getChapterContent(file){
 
 }
 
+app.listen(8000);
 console.log('ProGit Server Running on the port 8000');
