0bf1bbfd015578496b1342687a0463f0b4a84f5e
iissnan <iissnan@gmail.com>
2014-09-06 12:33:05 +0800

    Using PJAX + NProgress

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,14 +1,6 @@
 (function () {
-    function getFileNameFromUri(uri) {
-        var pathName = uri.split("/");
-        if (pathName.length > 0) {
-            return pathName[pathName.length - 1];
-        }
-        return "";
-    }
-
     var currentFileName = getFileNameFromUri(window.location.pathname);
-    $(".sub-nav a").each(function() {
+    $(".sub-nav").find('a').each(function() {
         if (getFileNameFromUri(this.href) === currentFileName) {
             $(this)
                 .click(function(){
@@ -17,4 +9,21 @@
                 .parent().addClass("active");
         }
     });
+
+    function getFileNameFromUri(uri) {
+        var pathName = uri.split("/");
+        if (pathName.length > 0) {
+            return pathName[pathName.length - 1];
+        }
+        return "";
+    }
+
+    $(document)
+        .pjax('a', '#pjax-container', {fragment: '#pjax-container'})
+        .on('pjax:start', function () {
+            NProgress.start();
+        })
+        .on('pjax:end', function () {
+            NProgress.done();
+        });
 }());
